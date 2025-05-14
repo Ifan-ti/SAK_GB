01f6720a7cdb5b5306a6dfb6f5af8cfcefcd3551
Ifan-ti <e41241494@student.polije.ac.id>
2025-05-14 13:25:24 +0700

    ifan-Menambahkan fitur cetak nota

---
+++ src/ServiceReport/CetakNota.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ServiceReport;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author achmad ifan
+ */
+public interface CetakNota {
+    void cetakNota(JPanel pn, String Barcode);
+}
